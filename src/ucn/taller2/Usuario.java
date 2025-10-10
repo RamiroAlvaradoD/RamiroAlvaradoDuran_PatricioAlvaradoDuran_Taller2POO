0633ecbcb1d9d740eeb2c81186de727bad07999a
RamiroAlvaradoD <ramiro.alvarado@alumnos.ucn.cl>
2025-10-09 23:52:48 -0300

    Muchos cambios a lo largo de las clases
    Hice muchos cambios en varias clases porque estropee cosas actualizando el main en github, se incorporaron cambios hechos tanto por mí como por Patricio, que me pasó sus cambios por discord para establecer este commit como un checkpoint nuevo

--- src/ucn/taller2/Usuario.java
+++ src/ucn/taller2/Usuario.java
@@ -1,33 +1,30 @@
 package ucn.taller2;
 
 public class Usuario {
-    private final String username;
-    private final String hash; // contraseña en SHA-256 codificada en Base64
-    private final String rol;  // ADMIN o USER
+	private String username;
+	private String passHash;
+	private String rol;
+	
+	
+	public Usuario (String username, String passHash, String rol) {
+		this.username = username;
+		this.passHash = passHash;
+		this.rol = rol;
+	}
 
-    public Usuario(String username, String hash, String rol) {
-        this.username = username;
-        this.hash = hash;
-        this.rol = rol;
-    }
 
-    public String getUsername() {
-        return username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public String getHash() {
-        return hash;
-    }
 
-    public String getRol() {
-        return rol;
-    }
+	public String getPassHash() {
+		return passHash;
+	}
 
-    @Override
-    public String toString() {
-        return "Usuario{" +
-                "username='" + username + '\'' +
-                ", rol='" + rol + '\'' +
-                '}';
-    }
+
+	public String getRol() {
+		return rol;
+	}
+
 }
