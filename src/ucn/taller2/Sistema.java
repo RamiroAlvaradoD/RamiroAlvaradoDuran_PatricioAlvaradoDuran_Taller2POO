06ce1de1924ea247d7dcb66faec7b23aa3b706ed
Patricio Alvarado <Garaxlight@users.noreply.github.com>
2025-10-09 21:09:11 -0300

    Adicion de cambios en login oficial
    Se hacen los metodos de login y su categorizacion entre User y Admin, y en la interfaz se hace la conexion a Sistema

--- src/ucn/taller2/Sistema.java
+++ src/ucn/taller2/Sistema.java
@@ -1,5 +1,60 @@
 package ucn.taller2;
 
+import java.util.Base64;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.security.MessageDigest;
 public class Sistema {
 
+	
+	public boolean login(String usuario, String contraseña) {
+		try {
+			Scanner sc = new Scanner(new File("data/usuarios.txt"));
+			while(sc.hasNextLine()) {
+				String line = sc.nextLine();
+				String[] data = line.split(";");
+				String userStored = data[0];
+				String hashPass = data[1];
+				String userType = data[2];
+				if (userStored.equals(usuario)) {
+					String hashInput = hashSHA256Base64(contraseña);
+					sc.close();
+					return hashInput.equals(hashPass);
+				}
+			}
+		}catch(FileNotFoundException e) {
+			System.out.println("Error leyendo usuarios.txt");
+		}
+		return false;
+	}
+	private String hashSHA256Base64(String pass) {
+		try {
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		byte[] hash = md.digest(pass.getBytes());
+		return Base64.getEncoder().encodeToString(hash);
+		}
+		catch(Exception e) {
+			return null;
+		}
+	}
+	public String getRolUsuario(String usuario) {
+		try {
+	        File file = new File("data/usuarios.txt");
+	        Scanner sc = new Scanner(file);
+	        while (sc.hasNextLine()) {
+	            String linea = sc.nextLine();
+	            String[] datos = linea.split(";");
+	            String userStored = datos[0];
+	            if (userStored.equals(usuario) && datos.length >= 3) {
+	                sc.close();
+	                return datos[2]; // retorna el rol, ej. "ADMIN" o "USER"
+	            }
+	        }
+	        sc.close();
+	    } catch (Exception e) {
+	        System.out.println("Error leyendo usuarios.txt");
+	    }
+	    return null; // si no encuentra al usuario
+	}
 }
